c775a4630ef3d0d33d96b48cc73d13a16da8fd73
Warren Harmon <harmonw@cliu.org>
2025-11-26 12:42:49 -0500

    display "your SSG username" instead of "user_name"

--- include/user_name_required.php
+++ include/user_name_required.php
@@ -3,19 +3,19 @@
 $user_name = $_POST['user_name'] ?? $_GET['user_name'] ?? "";
 if (! $user_name) {
 	?>
-	<h2 style='color:red'>Error: no user_name passed</h2>
-	Please <a href="index.php?data_month=<?=$data_month?>">choose an Upload Username for the report</a>
+	<h2 style='color:red'>Error: no SSG Username passed</h2>
+	Please <a href="index.php?data_month=<?=$data_month?>">enter your SSG username for the report</a>
 	<?php
 	exit();
 } elseif (! preg_match('/^[A-Z]*$/', $user_name)) {
 	?>
-	<h2 style='color:red'>Error: Invalid 'user_name'</h2>
-	Please <a href="index.php?data_month=<?=$data_month?>">choose an Upload Username for the report</a>
+	<h2 style='color:red'>Error: invalid SSG Username passed</h2>
+	Please <a href="index.php?data_month=<?=$data_month?>">enter your SSG username for the report</a>
 	<?php
 	exit();
 } else {
 	?>
-	<h2 style='color:green'>UserID for Upload: <?=$user_name?></h2>
+	<h2 style='color:green'>Your SSG Username: <?=$user_name?></h2>
 	<?php
 }
 ?>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $data_month = $_POST['data_month'] ?? $_GET['data_month'] ?? $default_data_month
 <table>
 <form action="upload.php" method="get" enctype="multipart/form-data">
 	<tr><td align="right">Month for data (YYYY-MM)</td><td><input type="text" name="data_month" value="<?=$data_month?>"></td></tr>
-	<tr><td align="right">UserID for Upload</td><td><input type="text" name="user_name" value=""></td></tr>
+	<tr><td align="right">Your SSG Username</td><td><input type="text" name="user_name" value=""></td></tr>
 	<tr><td colspan="2" align="right"><input type="submit" name="submit" value="Submit"></td></tr>
 </form>
 </table>
