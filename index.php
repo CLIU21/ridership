c95ca7f7f784a1a2bc6cf66e9fa7a1c3848e2ef0
Warren Harmon <harmonw@cliu.org>
2025-12-02 10:49:55 -0500

    no "enctype=multipart" when not uploading files

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $default_data_month = date('Y-m', $last_month);
 $data_month = $_POST['data_month'] ?? $_GET['data_month'] ?? $default_data_month;
 ?>
 <table>
-<form action="upload.php" method="get" enctype="multipart/form-data">
+<form action="upload.php" method="get">
 	<tr><td align="right">Month for data (YYYY-MM)</td><td><input type="text" name="data_month" value="<?=$data_month?>"></td></tr>
 	<tr><td align="right">Your SSG Username</td><td><input type="text" name="user_name" value=""></td></tr>
 	<tr><td colspan="2" align="right"><input type="submit" name="submit" value="Submit"></td></tr>
