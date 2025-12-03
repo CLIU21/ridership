0563b0977537184c1ff4791c8fb96d8251e26f4b
Warren Harmon <harmonw@cliu.org>
2025-12-03 16:00:53 -0500

    fix the output filename

--- include/file_paths_export.php
+++ include/file_paths_export.php
@@ -4,6 +4,30 @@ function is_export_path($full_path) {
 	return preg_match('/^test_excel_output_(EI|SA)_[0-9]+.xlsx$/', $basename);
 }
 
+function get_month_number($data_dir) {
+	// echo "DEBUG: get_month_number($data_dir)<br/>\n";
+	$data_month = basename($data_dir);
+	echo "DEBUG: data_month = $data_month<br/>\n";
+	return "10";
+}
+
+function get_grade_name($grade) {
+	echo "DEBUG: get_grade_name($grade)<br/>\n";
+	$grade_lookup = [
+		'EI' => 'etr',
+		'SA' => 'tr',
+	];
+	$grade_name = $grade_lookup[$grade] ?? "";
+	if (! $grade_name) {
+		throw new Exception("INVALID GRADE '$grade'");
+	}
+	return $grade_name;
+}
+
 function export_file_path($data_dir, $grade, $i) {
-	return "{$data_dir}/test_excel_output_{$grade}_{$i}.xlsx";
+	$month_number = get_month_number($data_dir);
+	$grade_name = get_grade_name($grade);
+	$i += 1;
+
+	return "{$data_dir}/{$month_number}_Ridership_{$grade_name}_part_{$i}.xlsx";
 }
