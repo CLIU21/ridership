bc5e85bd916cb0e8492e4b965745c68998304f4e
Warren Harmon <harmonw@cliu.org>
2025-12-04 15:48:07 -0500

    new secrets for example file

--- include/secrets-EXAMPLE.php
+++ include/secrets-EXAMPLE.php
@@ -2,3 +2,8 @@
 $client_id = "SECRET";  //Azure Application (client) ID
 $ad_tenant = "SECRET";  //Azure Active Directory Tenant ID, with Multitenant apps you can use "common" as Tenant ID, but using specific endpoint is recommended when possible
 $client_secret = "SECRET";  //Azure App Client Secret value, remember that this expires someday unless you haven't set it not to do so
+
+$mysql_hostname = "SECRET";	// hostname for MySQL instance
+$mysql_username = "SECRET";	// username for MySQL instance
+$mysql_password = "SECRET";	// password for MySQL instance
+$mysql_database = "SECRET";	// password for MySQL instance
