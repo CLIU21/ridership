36204abfb81dde7d8d12c9c9bf7b71c6f21db299
Warren Harmon <harmonw@cliu.org>
2025-12-11 16:11:46 -0500

    delete unused column indexes

--- include/zpass_data_conversion.php
+++ include/zpass_data_conversion.php
@@ -15,16 +15,6 @@ define('ZPASS_STUDENT_INDEX', 17);
 define('ZPASS_DISTRICT_INDEX', 19);
 define('ZPASS_GRADE_INDEX', 20);
 
-define('ZPASS_TIME_INDEX', 'time'); 
-define('ZPASS_DAY_INDEX', 'day'); 
-define('ZPASS_HOURS_INDEX', 'hours'); 
-define('ZPASS_MIN_INDEX', 'min'); 
-define('ZPASS_MAX_INDEX', 'max'); 
-define('ZPASS_ELAPSED_INDEX', 'elapsed'); 
-define('ZPASS_COUNT_INDEX', 'count');
-define('ZPASS_SERVICE_NAME_INDEX', 'service_name'); 
-define('ZPASS_SERVICE_CODE_INDEX', 'service_code'); 
-
 function extract_relevant_columns($data) {
 	$column_list = [
 		ZPASS_LAST_NAME_INDEX,
