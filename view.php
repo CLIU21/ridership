bb1409b3c6964f4c11347fe41ef43c9251a5092d
Warren Harmon <harmonw@cliu.org>
2025-12-12 09:32:04 -0500

    read data_dir and user_name, but don't require them

--- view.php
+++ view.php
@@ -3,7 +3,9 @@ require_once "include/az.php";
 require_once "include/header.php";
 
 require_once "include/data_month_required.php";
-require_once "include/data_dir_required.php";
+require_once "include/user_name.php";
+
+require_once "include/data_dir_create.php";
 
 require_once "include/file_paths_import.php";
 require_once "include/file_paths_export.php";
