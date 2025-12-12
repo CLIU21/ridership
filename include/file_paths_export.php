86e26e131cf70b975cbe93c3089afd80821ad6ae
Warren Harmon <harmonw@cliu.org>
2025-12-12 16:12:55 -0500

    create error_file logic

--- include/file_paths_export.php
+++ include/file_paths_export.php
@@ -30,5 +30,21 @@ function export_file_path($data_dir, $grade, $i) {
 
 function is_export_path($full_path) {
 	$basename = basename($full_path);
-	return preg_match('/^[0-9]+_Ridership_(etr|tr)_part_[0-9]+.xlsx$/', $basename);
+	return preg_match('/^[0-9]+_Ridership_(etr|tr)_part_[0-9]+[.]xlsx$/', $basename);
 }
+
+function error_file_path($data_dir, $grade, $error_type) {
+	$month_number = get_month_number($data_dir);
+
+	if ($grade) {
+		return "{$data_dir}/ERROR_{$grade}_{$error_type}.xlsx";
+	} else {
+		return "{$data_dir}/ERROR_{$error_type}.xlsx";
+	}
+}
+
+function is_error_path($full_path) {
+	$basename = basename($full_path);
+	return preg_match('/^ERROR_.*[.]xlsx$/', $basename);
+}
+
