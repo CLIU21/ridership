ec239ca9ba23c172a139bdbcfa9e222ac6c177fa
Warren Harmon <harmonw@cliu.org>
2025-12-12 10:19:39 -0500

    remove success message from MySQL connect fn

--- include/mysql_connect.php
+++ include/mysql_connect.php
@@ -2,7 +2,7 @@
 
 require_once "include/secrets.php";
 
-echo "<br/>Connecting to MySQL ...\n";
+// echo "<br/>Connecting to MySQL ...\n";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_database);
@@ -11,6 +11,6 @@ $mysqli = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mys
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit();
-} else {
-	echo "Success.<br/><br/>\n";
+// } else {
+// 	echo "Success.<br/><br/>\n";
 }
