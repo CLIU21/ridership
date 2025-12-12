8623b0f25e71333299e5031290442b3f6ece1945
Warren Harmon <harmonw@cliu.org>
2025-12-12 12:50:04 -0500

    correct variables for uploaded data

--- view.php
+++ view.php
@@ -47,7 +47,7 @@ if ($checksum) {
 <h2>Data uploaded by user:</h2>
 <table border="1">
 	<?php
-	foreach ($uploaded_data as $label => $file_id) {
+	foreach ($uploaded_data as $file_id => $value) {
 		$label = $file_labels_import[$file_id];
 		?>
 	<tr><td style="font-weight: bold;"><?=$label?></td><td><?=$value?></td></tr>
