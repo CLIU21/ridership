d2d3887d9124cadd4c564c267ff17cef4bebc5bb
Warren Harmon <harmonw@cliu.org>
2025-12-16 10:01:01 -0500

    refactor: move CSS to its own file

--- include/header.php
+++ include/header.php
@@ -5,17 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ridership</title>
 	<script src="./js/ridership.js" type="text/javascript"></script>
-
-<!-- move this to a CSS file: -->
-<style>
-table.bordered {
-   border-collapse: collapse;
-}
-table.bordered td,  table.bordered th {
-   border: 2px solid black;
-}
-</style>
-
+	<link href="./css/ridership.css" rel="stylesheet" />
 </head>
 <body>
 <h1>Ridership Report</h1>
