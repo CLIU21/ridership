d8f6c382d2c7dfccdd3eb2b8150376aa55a9da27
Warren Harmon <harmonw@cliu.org>
2025-12-17 15:16:56 -0500

    comment out debug statements

--- include/split_data_at_row_count.php
+++ include/split_data_at_row_count.php
@@ -3,17 +3,21 @@ require_once "include/split_header_and_body.php";
 
 // input: matrix; output: array[matrix]
 function split_data_at_row_count($data, $max_rows) {
-	echo "DEBUG: split_data_at_row_count(" . count($data) . ", " . $max_rows . ")<br />\n";
+	// $size = count($data) - 1;
+	// echo "split_data_at_row_count({$size}, {$max_rows})<br />\n";
 
 	$output = [];
 	if (count($data) <= $max_rows) {
 		array_push($output, $data);
+		// echo "no split required<br />\n";
 	} else {
 		list($header, $body) = header_and_body($data);
 		$array_of_body_chunks = array_chunk($body, $max_rows);
-		foreach ($array_of_body_chunks as $one_body_chunk) {
+		foreach ($array_of_body_chunks as $i => $one_body_chunk) {
 			array_unshift($one_body_chunk, $header);
 			array_push($output, $one_body_chunk);
+			// $size = count($one_body_chunk) - 1;
+			// echo "file #{$i}: size {$size}<br />\n";
 		}
 	}
 	return $output;
